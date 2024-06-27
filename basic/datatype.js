7b7bf101527b96da7b1d98eb83e27c945f0fc4af
sameeksha rai <158418707+Samee28@users.noreply.github.com>
2024-06-27 12:47:32 +0000

    datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,59 @@
+"use strict";//treat all js code as  newer version
+//alert(3+3) //we are using node js not browser
+//coderealibity shuld be most prioprity
+console.log("samee");
+
+let name="hitesh";
+let age=24;
+let isloggedin=false;
+let state;
+
+
+//number=>2 to power  53
+//bigint//for big values
+
+//string=""
+//boolean=true/false
+//null=>standalone   value
+//undefined=>nalue not defined
+    //symbol=>for unique find
+
+    //object
+
+
+    console.log(typeof null);//object type hotav  nulll ka but undefined ka undefined hi hota hai
+
+
+/*NUMBER CONVERSION */
+let score="abc33";
+console.log(typeof score);
+console.log(typeof (score));
+//number convert
+let valueinnum=Number(score);
+console.log(typeof valueinnum);
+console.log(valueinnum);
+
+/*note:-
+"33"=>33
+"33abc"=NaN
+true=>1; false=0;
+*/
+//for boolean
+let  islogged="";
+
+let booleanconvert=Boolean(islogged);
+console.log(booleanconvert);
+
+//when using
+/* empty string=""  =converter boolean is false
+ 1=> true  0=>false
+ samee=true
+ */
+
+
+ let  somenum=33;
+
+let Stringconvert=String(somenum);
+console.log(Stringconvert);
+console.log(typeof Stringconvert);
+
