bdca0f6b16c8f4b7a72bf41199ccacb859334299
sameeksha rai <158418707+Samee28@users.noreply.github.com>
2024-06-28 19:24:52 +0000

    summary

---
+++ basic/datasummary.js
@@ -0,0 +1,22 @@
+//datatypes  *****1.primitive (not driectly copy) 2. non-primitive
+//store &&access ka base p diff kiya ha
+
+
+/*primritive---(call by value)//original nhi copy m changes karte ha
+.....7types of primitive datadtypes.....
+1-String
+2-Number
+3-Boolean
+4-null
+5-undefined
+6-symbol
+7-BigInt
+
+
+//Reference type(non-primitivee)//memory ma directly access avaliable
+1-Array
+2-Object
+3-functions
+*/
+
+//note:-JavaScript is a dynamic language and not static, which means that variables can hold values of different types during runtime
